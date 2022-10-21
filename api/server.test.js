c3b49589a8f95f7d2258190f5eeee991404b5b63
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2022-10-21 04:31:39 -0400

    red

--- api/server.test.js
+++ api/server.test.js
@@ -1,17 +1,35 @@
 // Write your tests here
-const require = require('supertest')
-const request = require('./server')
-const server = require('./server')
+const request = require('supertest')
 const router = require('./auth/auth-router')
+
+const server = require('../api/server')
+const db = require('../data/dbConfig')
+const bcrypt = require('bcryptjs')
+//const jwtDecode = require('jwt-decode')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
+
 test('sanity', () => {
   expect(true).toBe(true)
 })
 
 describe('./register endpoint', ()=>{
-  test('[1]', ()=>{
-    return request(router).post('/login')
+  test('[1]', async ()=>{
+
+    const res = await request(server).post('/api/auth/register').send({ username: 'foo', password: '1234' })
+    expect(res.status).toBe(200)
   })
-  test('[2]', ()=>{})
+  test('[2]', async ()=>{
+    const res = await request(server).post('/api/auth/register').send({ username: 'foo', password: '' })
+    expect(res.status).toBe(422)
+  })
 })
 
 describe('./login endpoint', ()=>{
