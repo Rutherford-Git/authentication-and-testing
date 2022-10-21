6077bdf6aa56e988378a80ff4fe036438ce853fb
Rutherford Alexis <rutherford.aalexis@Gmail.com>
2022-10-21 06:38:14 -0400

    no a

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,12 +6,12 @@ const secrets = require('../secret')
 const { userNameTaken, missing } = require('../middleware/restricted')
 
 
-router.post('/register', userNameTaken, missing, async (req, res, next) => {
+router.post('/register', userNameTaken, missing, (req, res, next) => {
 try {
   const { username, password } = req.body;
   const hash = bcryptjs.hashSync(password, 8)
   const newUser = { username, password: hash }
-  const result = await add(newUser)
+  const result = add(newUser)
   console.log(result)
   res.status(200).json(result)
 } catch(err) {
